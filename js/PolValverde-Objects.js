499aee9f6982bf90b5b74a52562b1ffdc0fee846
Pol Valverde i Valverde <pol.valverde.valverde@gmail.com>
2018-03-21 16:20:09 +0100

    New skecth: rainbow phyllotaxis

---
+++ js/PolValverde-Objects.js
@@ -0,0 +1,66 @@
+class RandomWords {
+
+    constructor(seed) {
+        this.seed = seed;
+        this.words = [];
+        this.words.push(this.seed[Math.floor(Math.random() * this.seed.length)]);
+    }
+
+    update() {
+        this.similars = RiTa.similarByLetter(this.words[this.words.length - 1]);
+        this.randomIndex = Math.floor(Math.random() * this.similars.length);
+        if (this.words.length === 1) this.words[0] = `<b><i>${this.words[0]}</i></b>`;
+        this.words.push(this.similars[this.randomIndex]);
+    }
+
+    print(container, prefix, sufix) {
+        this.prefix = prefix;
+        this.sufix = sufix;
+        this.text = this.words.reduce((text, word) => `${text} ${word}`);
+        $(container).html(`${prefix} ${this.text} ${sufix}`);
+    }
+}
+
+class dot {
+
+    constructor(array, colors, diameter, distanceIndex, max, angle) {
+        this.n = 0;
+        if (!array[0]) this.id = 0;
+        else this.id = array[array.length-1].id + 1;
+        this.x = width / 2;
+        this.y = height / 2;
+        this.colors = colors;
+        this.indexColor = this.id % this.colors.length;
+        this.r = this.colors[this.indexColor].r;
+        this.g = this.colors[this.indexColor].g;
+        this.b = this.colors[this.indexColor].b;
+        this.diameter = diameter;
+        this.c = distanceIndex;
+        this.max = max;
+        if (!angle) this.angle = 137.5;
+        else this.angle = angle;
+    }
+
+    update(array) {
+        if (this.n === this.max) {
+            array.shift();
+        } else {
+            // Dot angle, position multiplied by the default angle
+            this.a = this.n * this.angle;
+            // Radius, distance between the center of the skecth and the dot
+            this.rad = this.c * sqrt(this.n);
+            // x position, radius times the cosinus of the poiint angle
+            this.x = this.rad * cos(this.a) + width / 2;
+            // y position, radius times the sinus of the poiint angle
+            this.y = this.rad * sin(this.a) + height / 2;
+            // Update n position
+            this.n++;
+        }
+    }
+
+    draw() {
+        fill(this.r, this.g, this.b);
+        noStroke();
+        ellipse(this.x, this.y, this.diameter);
+    }
+}
--- js/sketch.js
+++ js/sketch.js
@@ -1,26 +1,124 @@
-let words = [];
-let initials = ["Pol", "Valverde"];
-let random;
+let spiral = Array(1);
+let ratio;
+let max;
+let diameter;
+
+let div;
+
+let song;
+let time;
+
+let background;
+let execute;
+let sequence = [1, 0, 0, 0];
+let loop = false;
+
+let sign;
+
+let colors = [{
+    r: 255,
+    g: 0,
+    b: 0
+}, {
+    r: 226,
+    g: 87,
+    b: 30
+}, {
+    r: 255,
+    g: 127,
+    b: 0
+}, {
+    r: 255,
+    g: 255,
+    b: 0
+}, {
+    r: 0,
+    g: 255,
+    b: 0
+}, {
+    r: 150,
+    g: 191,
+    b: 51
+}, {
+    r: 0,
+    g: 0,
+    b: 255
+}, {
+    r: 75,
+    g: 0,
+    b: 130
+}, {
+    r: 139,
+    g: 0,
+    b: 255
+}];
+
+function preload() {
+    soundFormats('wav');
+    song = loadSound("./assets/song.wav");
+    background = new p5.Phrase('background', changeBackground, sequence);
+    execute = new p5.Part();
+    execute.addPhrase(background);
+    execute.setBPM(140);
+}
 
 function setup() {
-    random = new RandomWords(initials);
+    let canvas = createCanvas(windowWidth, windowHeight);
+    angleMode(DEGREES);
+    canvas.parent('hola');
+
+    max = 2000;
+    ratio = 8;
+    diameter = 10;
+
+    spiral.fill(new dot(spiral, colors, diameter, ratio, max));
+
+    div = createDiv("Pol Valverde!");
+    div.style("color", "white");
+    div.style("font-size", "95px");
+    div.style("font-family", "Source Code Pro");
+    div.style("font-weight", "800");
+
+    sign = createDiv('<b>Pol Valverde i Valverde</br>Interaction Designer</b></br>Coded with <a href="https://p5js.org" target="_blank">p5.js </a>');
+    sign.style("color", "white");
+    sign.style("font-size", "18px");
+    sign.style("font-family", "Source Code Pro");
+    sign.style("font-weight", "400");
+
+    song.loop();
 }
 
 function draw() {
-    if (!final()) {
-        random.update();
-        random.print('#hola', "/*", "*/");
-        if (middle()) $('body').css("background-color", "rgb(255, 255, 255)");
-    } else {
-        $('#hola').append(" <b><i>Pol Valverde i Valverde / Interaction Designer</i></b>");
-        noLoop();
+    time = song.currentTime();
+    clear();
+
+    spiral.map(dot => dot.update(spiral));
+    spiral.map(dot => dot.draw())
+    spiral.push(new dot(spiral, colors, diameter, ratio, max));
+
+    if (time > 27.5) {
+        sign.style("color", "white");
+        if (loop == false) {
+            execute.loop();
+            loop = true;
+        }
+    } else if (time <= 27.5) {
+        div.center();
+        sign.style("color", "red");
+        sign.position(30,30);
+        execute.stop();
+        $('body').css("background-color", `white`);
+        loop = false;
+        fill(255,0,0);
     }
+
 }
 
-function middle() {
-    return ($('#hola').height() > ($('.hola').height()) / 2);
-}
+function changeBackground() {
+    let randomColor = Math.floor(Math.random() * colors.length);
+    $('body').css("background-color", `rgb(${colors[randomColor].r},${colors[randomColor].g},${colors[randomColor].b}`);
+    div.position(Math.random() * width, Math.random() * height);
+    sign.position(Math.random() * width, Math.random() * height);
 
-function final() {
-    return ($('#hola').height() >= ($('.hola').height()) - 40);
+
 }
